37e269ed1ad9e894bac127512d2f1b405a7dcfc6
Bedrock123 <info@builtbykingwilly.com>
2018-03-14 21:50:04 -0400

    Added in cleancult heder

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ class App extends Component {
   render() {
     return (
       <section className="App">
-        <h1>cleancult</h1>
+        <div className="content">
+          <h1>cleancult</h1>
+          <p>
+            Sorry the cleancult site is under maintenance right now. We should
+            have it up and running within the hour.
+          </p>
+        </div>
       </section>
     );
   }
